bf899a09b890142ff4d47de5e135e8f88092dae2
Dane Springmeyer <dane@mapbox.com>
2014-10-31 19:48:56 -0400

    more work at testing all apps

--- all.sh
+++
@@ -1,8 +0,0 @@
-for b in $(git for-each-ref --sort=-committerdate refs/remotes --format='%(refname:short)'); do
-    echo $b
-    git checkout $b
-    if [ -f ./script.sh ]; then
-        ./script.sh install
-    fi
-done
-#git checkout master
---
+++ install-all.sh
@@ -0,0 +1,15 @@
+if [ ! -d ~/.mason ]; then
+    git clone --depth=1 https://github.com/mapbox/mason.git ~/.mason
+    alias mason='~/.mason/mason'
+fi
+
+rm -rf test-all
+git clone --quiet https://github.com/mapbox/mason.git test-all
+cd test-all
+for b in $(git for-each-ref --sort=-committerdate refs/remotes --format='%(refname:short)'); do
+    git checkout --quiet $b
+    if [ -f ./script.sh ]; then
+        echo $b
+        ./script.sh install
+    fi
+done
